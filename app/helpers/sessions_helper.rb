dc08b99515c0c3e43d87dacbf5a389dedb36b46a
shuma1020 <shumaseki40@gmail.com>
2019-11-24 19:46:16 +0900

    Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,18 +9,22 @@ module SessionsHelper
         cookies.permanent.signed[:user_id] = user.id
         cookies.permanent[:remember_token] = user.remember_token
       end
-    # 現在ログイン中のユーザーを返す (いる場合)
-  def current_user
+
+        # 渡されたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
+   # 現在ログイン中のユーザーを返す (いる場合)
+   def current_user
     if (user_id = session[:user_id])
-        @current_user ||= User.find_by(id: user_id)
-      elsif (user_id = cookies.signed[:user_id])
-        
-        user = User.find_by(id: user_id)
-        if user && user.authenticated?(cookies[:remember_token])
-          log_in user
-          @current_user = user
-        end
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(:remember, cookies[:remember_token])
+        log_in user
+        @current_user = user
       end
+    end
   end
   # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
@@ -40,4 +44,15 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+  # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token
-    before_save { self.email = email.downcase }
+  has_many :microposts, dependent: :destroy
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
     validates :name,  presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
     format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
     has_secure_password
-    validates :password, presence: true, length: { minimum: 6 }
+    validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+    
 
     # 渡された文字列のハッシュ値を返す
   def User.digest(string)
@@ -27,14 +31,38 @@ class User < ApplicationRecord
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
-   # 渡されたトークンがダイジェストと一致したらtrueを返す
-   def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+   # トークンがダイジェストと一致したらtrueを返す
+   def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
+
+  
     
    # ユーザーのログイン情報を破棄する
    def forget
     update_attribute(:remember_digest, nil)
   end
+
+    # 試作feedの定義
+  # 完全な実装は次章の「ユーザーをフォローする」を参照
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
+
+  private
+
+  # メールアドレスをすべて小文字にする
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  # 有効化トークンとダイジェストを作成および代入する
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
+
 end
